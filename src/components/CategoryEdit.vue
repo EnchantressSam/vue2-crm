65c71eff1e2c2c201631fd7ac34cf0cf7833191f
Tatiana Loginova <tatyanka8samara@mail.ru>
2020-06-23 19:00:58 +0400

    Start layouts, date, vuelidate, Firebase. Added users & categories.

---
+++ src/components/CategoryEdit.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="col s12 m6">
+    <div>
+      <div class="page-subtitle">
+        <h4>Редактировать</h4>
+      </div>
+
+      <form>
+        <div class="input-field">
+          <select ref="select">
+            <option v-for="c of categories" :key="c.id" :value="c.id">{{c.title}}</option>
+          </select>
+          <label>Выберите категорию</label>
+        </div>
+
+        <div class="input-field">
+          <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{invalid: $v.title.$dirty && !$v.title.required}"
+          />
+          <label for="name">Название</label>
+          <span
+            class="helper-text invalid"
+            v-if="$v.title.$dirty && !$v.title.required"
+          >Введите название категории</span>
+        </div>
+
+        <div class="input-field">
+          <input
+            id="limit"
+            type="number"
+            v-model.number="limit"
+            :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
+          />
+          <label for="limit">Лимит</label>
+          <span
+            class="helper-text invalid"
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+          >Минимальное значение {{$v.limit.$params.minValue.min}}</span>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit">
+          Обновить
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    select: null
+  }),
+  mounted() {
+    this.select = window.M.FormSelect.init(this.$refs.select);
+  },
+  destroyed() {
+    if (this.select && this.select.destroy()) {
+      this.select.destroy();
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,45 @@
 import Vue from "vue";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import dateFilter from "@/filters/date.filter";
+import currencyFilter from "@/filters/currency.filter";
+import messagePlugin from "./utils/message.plugin";
+import Loader from "@/components/app/Loader";
+import "./registerServiceWorker";
+import "materialize-css/dist/js/materialize.min";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+Vue.use(messagePlugin);
+Vue.use(Vuelidate);
+Vue.filter("date", dateFilter);
+Vue.filter("currency", currencyFilter);
+Vue.component("Loader", Loader);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyADKRE4-G1-jOriYNZkUeD_2P6B3RKF6EY",
+  authDomain: "vue-crm-20.firebaseapp.com",
+  databaseURL: "https://vue-crm-20.firebaseio.com",
+  projectId: "vue-crm-20",
+  storageBucket: "vue-crm-20.appspot.com",
+  messagingSenderId: "476290750365",
+  appId: "1:476290750365:web:2713ed9e40b43fff454174",
+  measurementId: "G-8B9QLT9JPQ",
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});
